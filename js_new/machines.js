a2f18a8db32ac1ede64659d2eb70da08e5c2f3ce
usaprojectapps-test <usaprojectapps@gmail.com>
2026-01-15 15:17:16 -0500

    update

--- js_new/machines.js
+++ js_new/machines.js
@@ -5,7 +5,7 @@ const SUPABASE_URL = "https://kjfzdmmloryzbuiixceh.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZnpkbW1sb3J5emJ1aWl4Y2VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3OTkyODQsImV4cCI6MjA4MzM3NTI4NH0.ivmARw-Nj0kegWTV3GZwbyKeHx0c7eQRUtGww1S8B8M";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // ---------------------------------------------------------
 // USER CONTEXT
